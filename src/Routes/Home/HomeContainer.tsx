be7b53073117adfc97d28c43cf61e5096cf29839
tonky0110 <tonky0110@gmail.com>
2019-01-17 22:23:49 +0900

    2.32 HomePresenter 후 query추가해야함.(에러남)

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { RouteComponentProps } from "react-router-dom";
+import { Query } from "react-apollo";
+import { RouteComponentProps } from "react-router";
+import { userProfile } from "../../types/api";
 import HomePresenter from "./HomePresenter";
 
 interface IState {
@@ -8,18 +10,20 @@ interface IState {
 
 interface IProps extends RouteComponentProps<any> {}
 
+class ProfileQuery extends Query<userProfile> {}
+
 class HomeContainer extends React.Component<IProps, IState> {
   public state = {
     isMenuOpen: false
   };
-
   public render() {
     const { isMenuOpen } = this.state;
     return (
-      <HomePresenter isMenuOpen={isMenuOpen} toggleMenu={this.toggleMenu} />
+      <ProfileQuery query={USER_PROFILE}>
+        <HomePresenter isMenuOpen={isMenuOpen} toggleMenu={this.toggleMenu} />
+      </ProfileQuery>
     );
   }
-
   public toggleMenu = () => {
     this.setState(state => {
       return {
