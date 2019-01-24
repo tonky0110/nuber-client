1c85a45a3073323f7d155ca9c2ecece0968cc08d
tonky0110 <tonky0110@gmail.com>
2019-01-25 00:09:15 +0900

    2.48 add eventListener(Dragend) and getCenter()(lat(), lng())

--- src/Routes/FindAddress/FindAddressContainer.tsx
+++ src/Routes/FindAddress/FindAddressContainer.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom";
 import FindAddressPresenter from "./FindAddressPresenter";
 
 // interface IProps extends RouteComponentProps<any> {}
-
-class FindAddressContainer extends React.Component<any> {
+interface IState {
+  lat: number;
+  lng: number;
+}
+class FindAddressContainer extends React.Component<any, IState> {
   public mapRef: any;
   public map: google.maps.Map;
   constructor(props) {
@@ -18,14 +21,18 @@ class FindAddressContainer extends React.Component<any> {
     );
   }
   public render() {
-    console.log(this.props);
+    // console.log(this.props);
     return <FindAddressPresenter mapRef={this.mapRef} />;
   }
   public handleGeoSuccess = (position: Position) => {
     const {
       coords: { latitude, longitude }
     } = position;
-    console.log(position);
+    // console.log(position);
+    this.setState({
+      lat: latitude,
+      lng: longitude
+    });
     this.loadMap(latitude, longitude);
   };
   public handleGeoError = () => {
@@ -41,6 +48,17 @@ class FindAddressContainer extends React.Component<any> {
       zoom: 15
     };
     this.map = new maps.Map(mapNode, mapConfig);
+    this.map.addListener("dragend", this.handelDragEnd);
+  };
+  public handelDragEnd = () => {
+    const newCenter = this.map.getCenter();
+    const lat = newCenter.lat();
+    const lng = newCenter.lng();
+    console.log(lat, lng);
+    this.setState({
+      lat,
+      lng
+    });
   };
 }
 
